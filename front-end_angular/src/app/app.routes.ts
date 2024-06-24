a3fe60e93de400f0b241a4d7f57846f8daf3109c
Micael Santana <micaelsantana.dev@gmail.com>
2024-06-24 18:32:17 -0300

    add route about-me and add prop title in route login, cadastro, not-found, profile, about-me and policy of privacity

--- front-end_angular/src/app/app.routes.ts
+++ front-end_angular/src/app/app.routes.ts
@@ -10,28 +10,39 @@ export const routes: Routes = [
     path: 'not-found',
     loadComponent: () => import('./pages/not-found/not-found.component'),
     pathMatch: 'full',
+    title: 'Página não econtrada',
   },
   {
     path: 'auth-user/login',
     loadComponent: () => import('./pages/user/auth/login/login.component'),
     pathMatch: 'full',
+    title: 'Login',
   },
   {
     path: 'auth-user/cadastro',
     loadComponent: () =>
       import('./pages/user/auth/register/register.component'),
     pathMatch: 'full',
+    title: 'Cadastro',
   },
   {
     path: 'user/profile',
     loadComponent: () => import('./pages/user/profile/profile.component'),
     pathMatch: 'full',
+    title: 'Perfil',
+  },
+  {
+    path: 'about-me',
+    loadComponent: () => import('./pages/about-me/about-me.component'),
+    pathMatch: 'full',
+    title: 'Sobre Micael Santana',
   },
   {
     path: 'policy-of-privacity',
     loadComponent: () =>
       import('./pages/policy-of-privacity/policy-of-privacity.component'),
     pathMatch: 'full',
+    title: 'Política de Privacidade',
   },
   {
     path: 'post:id',
