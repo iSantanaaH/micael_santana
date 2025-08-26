15d40b9cf66e87364ebd394f19e51619506a211e
iSantanaaH <micaelsantana.dev@gmail.com>
2025-08-25 21:40:06 -0300

    create one function what calculate my age dynamically

--- front-end/src/app/pages/home/home-page.component.ts
+++ front-end/src/app/pages/home/home-page.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css', './home-page-responsive.css'],
 })
-export default class HomePageComponent {}
+export default class HomePageComponent {
+  calcMyAge(): number {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
+    const birthdate = '16/02/2001';
+    const [day, month, year] = birthdate.split('/').map(Number);
+
+    let age = currentYear - year;
+
+    const birthThisYear = new Date(currentYear, month - 1, day);
+    if (currentDate < birthThisYear) {
+      age--;
+    }
+
+    return age;
+  }
+}
