8537daaee395a8627b4b663bc432d313bf5b6138
iSantanaaH <micaelsantana.dev@gmail.com>
2025-08-25 21:50:02 -0300

    remove route about-me

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -6,13 +6,6 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/home/home-page.component'),
     pathMatch: 'full',
   },
-
-  {
-    path: 'about-me',
-    loadComponent: () => import('./pages/about-me/about-me.component'),
-    pathMatch: 'full',
-    title: 'Sobre',
-  },
   {
     path: '**',
     redirectTo: 'not-found',
