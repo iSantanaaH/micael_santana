b1abb9c4d38ac2906a7ebe4c1c37bd06365f41b6
iSantanaaH <micaelsantana.dev@gmail.com>
2025-10-29 02:00:11 -0300

    add karma-conf to use jasmine test in poject and add zone.js in pollyfils inside angular.json

---
+++ front-end/karma.conf.js
@@ -0,0 +1,26 @@
+// karma.conf.js
+module.exports = function (config) {
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false, // deixa o output visível no navegador
+    },
+    reporters: ["progress", "kjhtml"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ["ChromiumHeadless"], // 👈 roda sem abrir janela
+    singleRun: false,
+    restartOnFileChange: true,
+  });
+};
