0aae3d9a07eb06a394708d51f4b6ab122042f9d6
iSantanaaH <micaelsantana.dev@gmail.com>
2025-11-05 15:56:10 -0300

    change the form of calling the isMenuMobileOpen, new is a signal, should use parental

--- front-end/src/app/pages/home/home-page.component.spec.ts
+++ front-end/src/app/pages/home/home-page.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePageComponent } from './home-page.component';
+import HomePageComponent from './home-page.component';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
@@ -8,10 +8,9 @@ describe('HomePageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomePageComponent]
-    })
-    .compileComponents();
-    
+      imports: [HomePageComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +18,20 @@ describe('HomePageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should start with menu closed', () => {
+    expect(component.isMobileMenuOpen()).toBeFalse();
+  });
+
+  it('should block scroll when menu open', () => {
+    const buttonOpenMenuMobile =
+      fixture.nativeElement.querySelector('.icon-burguer');
+
+    buttonOpenMenuMobile.click();
+    fixture.detectChanges();
+
+    expect(component.isMobileMenuOpen()).toBeTrue();
+    expect(document.body.style.overflow).toBe('hidden');
   });
 });
