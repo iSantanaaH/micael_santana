69d5255ecbb879524d8667cd5e0d8be531a443d0
iSantanaaH <micaelsantana.dev@gmail.com>
2026-01-09 11:14:01 -0300

    define translate to use pt-BR in constructor and remove overflow 'hidden' to main. Add labelKey in navLinks and fragment to separate responsabilities. Create array flags to rendering flags of translate in template and create isDropdownLanguageOpen, toggleLanguageDropdown and changeLanguagePage

--- front-end/src/app/pages/home/home-page.component.ts
+++ front-end/src/app/pages/home/home-page.component.ts
@@ -1,18 +1,18 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component, effect, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, TranslateModule],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css', './home-page-responsive.css'],
 })
 export default class HomePageComponent {
-  constructor() {
-    effect(() => {
-      document.body.style.overflow = this.isMobileMenuOpen() ? 'hidden' : '';
-    });
+  constructor(private translate: TranslateService) {
+    this.translate.setFallbackLang('pt-BR');
+    this.translate.use('pt-BR');
   }
 
   calcMyAge(): number {
@@ -33,34 +33,73 @@ export default class HomePageComponent {
   }
 
   navLinks = [
-    { id: 'Home', label: 'home', icon: 'assets/Images/ui-icons/home.svg' },
-    { id: 'Sobre', label: 'about-me', icon: 'assets/Images/ui-icons/info.svg' },
     {
-      id: 'Habilidades',
-      label: 'skills',
+      id: 1,
+      labelKey: 'menu.home',
+      fragment: 'home',
+      icon: 'assets/Images/ui-icons/home.svg',
+    },
+    {
+      id: 2,
+      labelKey: 'menu.about',
+      fragment: 'about',
+      icon: 'assets/Images/ui-icons/info.svg',
+    },
+    {
+      id: 3,
+      labelKey: 'menu.skills',
+      fragment: 'skills',
       icon: 'assets/Images/ui-icons/rocket.svg',
     },
     {
-      id: 'Projetos',
-      label: 'projects',
+      id: 4,
+      labelKey: 'menu.projects',
+      fragment: 'projects',
       icon: 'assets/Images/ui-icons/project.svg',
     },
     {
-      id: 'Trajetoria',
-      label: 'trajectory',
+      id: 5,
+      labelKey: 'menu.trajectory',
+      fragment: 'trajectory',
       icon: 'assets/Images/ui-icons/map.svg',
     },
     {
-      id: 'Contato',
-      label: 'contact-me',
+      id: 6,
+      labelKey: 'menu.contact',
+      fragment: 'contact',
       icon: 'assets/Images/ui-icons/radio-tower.svg',
     },
   ];
 
+  flags = [
+    {
+      id: 1,
+      lang: 'Português',
+      labelKey: 'language.ptBR',
+      icon: 'assets/Images/ui-icons/flag-of-brazil.svg',
+    },
+    {
+      id: 2,
+      lang: 'Inglês',
+      labelKey: 'language.enUS',
+      icon: 'assets/Images/ui-icons/flag-of-united-states.svg',
+    },
+  ];
+
   isMobileMenuOpen = signal(false);
+  isDropdownLanguageOpen = signal(false);
 
   toggleMobileMenu(event: MouseEvent): void {
     event.stopPropagation();
     this.isMobileMenuOpen.update((isOpen) => !isOpen);
+  }
+
+  toggleLanguageDropdown(event: MouseEvent): void {
+    event.stopPropagation();
+    this.isDropdownLanguageOpen.update((isOpen) => !isOpen);
+  }
+
+  changeLanguagePage(lang: string): void {
+    this.translate.use(lang);
   }
 }
