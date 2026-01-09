6254647788b794ef90181f2ea9d3a348442d47ae
iSantanaaH <micaelsantana.dev@gmail.com>
2026-01-09 10:41:49 -0300

    instal ngx-translate/core to translate page and configure app.config to use ngx-translate

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -3,7 +3,22 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+
+    provideTranslateService({
+      fallbackLang: 'pt-BR',
+      loader: provideTranslateHttpLoader({
+        prefix: 'assets/i18n/',
+        suffix: '.json',
+      }),
+    }),
+  ],
 };
